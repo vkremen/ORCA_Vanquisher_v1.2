fddcf219ffeefd48102dc61ce2bdacc5cce8f60e
Vaclav Kremen <xkremen@cvut.cz>
2019-10-31 12:17:18 -0500

    Updated comments for better sharing.

--- ORCA_Vanquisher/Program.cs
+++ ORCA_Vanquisher/Program.cs
@@ -1,5 +1,5 @@
 ﻿/*
-ORCA_Vanquisher code for removing directories created by Medtronic ORCA or RC+S RDK
+ORCA_or_RC+S_Data_Vanquisher code for removing sub-directories created by Medtronic ORCA or Summit RC+S RDK
 ----------------------------------------------------------------------
 Versioning:
 ----------------------------
@@ -8,27 +8,32 @@ Removed unnecesary code and renamed to ORCA_Vanquisher when filemover was runnin
 It stopped the service and deleted folders.
 
 Current Version - 1.2
-This version is not stopping filemover service and just deletes the files.
-It suppose filemover to be stopped or ORCA uninstalled.
+This version is not stopping filemover service. It assumes that ORCA and filemover service
+is disabled. So this version 1.2 just deletes the files and sub-directories in directories
+that are specified in the config json file.
+It suppose the filemover service to be stopped or ORCA uninstalled.
 
 -----------------------------------------------------------------------
 Inputs:
-directories2delete.json file with content of directory paths that needs to be deleted:
+directories2delete.json file with content of directory paths their sub-directories and files need to be deleted.
+This file has to be placed in same directory as executable binary.
+The content of the file should something look like:
 {
   "Folder to Delete #1": "C:\\Medtronic",
   "Folder to Delete #2": "C:\\Medtronic"
 }
 
 Outputs:
-Deletes all directories specified in directories2delete, if they are not locked by another processes (e.g. filemover)
-
+The program deletes all sub-directories and files in directories that are spedified in directories2delete.json.
+If any of the directories doesnt' exist or is locked by another processes (e.g. filemover service),
+it will skip them.
 It produces a directoriesdeleted.json file, that contains log of last run of the code, e.g.:
 [{"Name":"C:\\Medtronic","Date":"10/30/2019 16:27","Success":"NOT successful"},{"Name":"C:\\Medtronic","Date":"10/30/2019 16:27","Success":"NOT successful"}]
 
 -------------------------------------------------------------------------
 Copyright (c) 2017-2019, Mayo Foundation for Medical Education and Research (MFMER), All rights reserved. Academic, non-commercial use of this software is allowed with expressed permission of the developers. MFMER and the developers disclaim all implied warranties of merchantability and fitness for a particular purpose with respect to this software, its application, and any verbal or written statements regarding its use. The software may not be distributed to third parties without consent of MFMER. Use of this software constitutes acceptance of these terms and acceptance of all risk and liability arising from the software’s use.
 
-Contributors: Daniel Crepeau, Tal Pal Attia, Jan Cimbalnik, Hari Guragain, Mona Nasseri, Vaclav Kremen, Benjamin Brinkmann, Matt Stead, Gregory Worrell.
+Contributors: Vaclav Kremen, Gregory Worrell.
 
 FileMoverStartStop done by Vaclav Kremen: kremen.vaclav@mayo.edu
 --------------------------------------------------------------------------
@@ -45,11 +50,12 @@ namespace StartStopFilemover
 {
     class Program
     {
+        // make public class for list with directories and its attributes
         public class DirectoryDeleted
        {
-            public string Name { get; set; }
-            public string Date { get; set; }
-            public string Success { get; set; }
+            public string Name { get; set; } // name of the directory
+            public string Date { get; set; } // date when program tried to delete its sub-directories
+            public string Success { get; set; } // was deletion successful or not
         }
 
         static void Main(string[] args)
